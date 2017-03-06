4a0639b0ab8d0b30c6ac81cef8bd6f9a93ae38a8
imsanjeev4 <sanjeev.gupta@in.hanusoftware.com>
2017-03-06 14:34:48 +0530

    #6 File upload functionality added

--- Jobportal/app/models.py
+++ Jobportal/app/models.py
@@ -15,14 +15,48 @@ class Register(models.Model):
     class Meta:
         db_table = "registered_users"
 		
-class Document(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-	
-    def __str__(self):
-        return self.description
-		
-    class Meta:
-        db_table = "upload_data"
+class CandidateInfo(models.Model):
+	username = models.CharField(max_length=50)
+	email = models.CharField(max_length=70, null=True, blank=True, unique=True)
+	phone = models.CharField(max_length=50)
+	website = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
+	designation = models.CharField(max_length=50)
+	experience = models.CharField(max_length=50)
+	age = models.CharField(max_length=50)
+	current_salary = models.CharField(max_length=50)
+	expected_salary = models.CharField(max_length=50)
+	edulevel = models.CharField(max_length=50)
+	uploadcv = models.CharField(max_length=255, blank=True)
+	document = models.FileField(upload_to='documents/')
+	uploaded_at = models.DateTimeField(auto_now_add=True)
+	aboutme = models.CharField(max_length=50)
+	skill_name = models.CharField(max_length=50)
+	skill_level = models.CharField(max_length=50)
+	degree_name = models.CharField(max_length=50)
+	degree_date = models.CharField(max_length=50)
+	about_degree = models.CharField(max_length=50)
+	company = models.CharField(max_length=50)
+	company_website = models.CharField(max_length=50)
+	join_from = models.CharField(max_length=50)
+	endon= models.CharField(max_length=50)
+	location = models.CharField(max_length=50)
+	about_company = models.CharField(max_length=50)
+	project_name = models.CharField(max_length=50)
+	project_start = models.CharField(max_length=50)
+	project_end = models.CharField(max_length=50)
+	project_desc = models.CharField(max_length=50)
+	project_file = models.CharField(max_length=255, blank=True)
+	project_document = models.FileField(upload_to='documents/')
+	project_uploaded_at = models.DateTimeField(auto_now_add=True)
+	facebook = models.CharField(max_length=50)
+	twitter = models.CharField(max_length=50)
+	google_plus = models.CharField(max_length=50)
+	linkedin = models.CharField(max_length=50)
+	pinterest = models.CharField(max_length=50)
+	behance = models.CharField(max_length=50)
+	def __str__(self):
+		return self.description
+	class Meta:
+		db_table = "PersonalInfo"
 # Create your models here.
--- Jobportal/app/views.py
+++ Jobportal/app/views.py
@@ -6,7 +6,8 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from app.models import Register, Document
+from django.core.files.storage import FileSystemStorage
+from app.models import Register, CandidateInfo
 import hashlib
 def index(request):
     return render_to_response("index.html", context_instance = RequestContext(request))
@@ -74,6 +75,89 @@ def listview(request):
     return render_to_response("candidate-listing.html", context_instance=RequestContext(request))
 
 def profile(request):
-    return render_to_response("edit-profile.html", context_instance = RequestContext(request))
+	if request.method == 'POST':
+		username = request.POST['name']
+		email = request.POST['email']
+		phone = request.POST['phone']
+		website = request.POST['website']
+		address = request.POST['address']
+		designation = request.POST['designation']
+		experience = request.POST['experience']
+		age = request.POST['age']
+		current = request.POST['current']
+		demand = request.POST['demand']
+		edulevel = request.POST['edulevel']
+		uploadcv = request.FILES['uploadcv']
+		fs = FileSystemStorage()
+		filename_cv = fs.save(uploadcv.name, uploadcv)
+		#uploaded_file_url = fs.url(filename)
+		aboutme = request.POST['aboutme']
+		skill = request.POST['skill']
+		skilllevel = request.POST['skilllevel']
+		degreename = request.POST['degreename']
+		degreedate = request.POST['degreedate']
+		aboutdeg = request.POST['aboutdeg']
+		company = request.POST['company']
+		webcom = request.POST['webcom']
+		join_frm = request.POST['join_frm']
+		endon = request.POST['endon']
+		location = request.POST['location']
+		about_company = request.POST['about_company']
+		projname = request.POST['projname']
+		startfrm = request.POST['startfrm']
+		projendon = request.POST['endon2']
+		projdesc = request.POST['projdesc']
+		project_file = request.FILES['project_file']
+		projectfs = FileSystemStorage()
+		filename = projectfs.save(project_file.name, project_file)
+		#uploaded_file_url = fs.url(filename)
+		fb = request.POST['fb']
+		twitter = request.POST['twitter']
+		gplus = request.POST['gplus']
+		linkedin = request.POST['linkedin']
+		pinterest = request.POST['pinterest']
+		behance = request.POST['behance']
+		print(username,email,phone,website,address,designation,experience,age,current,demand,edulevel,uploadcv,project_file)
+		profileInfo = CandidateInfo(
+			username=username,
+			email=email,
+			phone=phone,
+			website=website,
+			address=address,
+			designation=designation,
+			experience=experience,
+			age=age,
+			current_salary=current,
+			expected_salary=demand,
+			edulevel=edulevel,
+			uploadcv=uploadcv,
+			aboutme=aboutme,
+			skill_name=skill,
+			skill_level=skilllevel,
+			degree_name=degreename,
+			degree_date=degreedate,
+			about_degree=aboutdeg,
+			company=company,
+			company_website=webcom,
+			join_from=join_frm,
+			endon=endon,
+			location=location,
+			about_company=about_company,
+			project_name=projname,
+			project_start=startfrm,
+			project_end=projendon,
+			project_desc=projdesc,
+			project_file=project_file,
+			facebook=fb,
+			twitter=twitter,
+			google_plus=gplus,
+			linkedin=linkedin,
+			pinterest=pinterest,
+			behance=behance		
+		)
+		profileInfo.save()
+		return render_to_response("edit-profile.html", context_instance = RequestContext(request))
+	else:
+		return render_to_response("edit-profile.html", context_instance = RequestContext(request))
 	
 # Create your views here.
